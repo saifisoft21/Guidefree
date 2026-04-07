9f2f1fd58e8a0c8b315f2592c623e9084a7c920e
saifisoft21 <zarvantalks@gmail.com>
2026-04-07 21:53:52 +0100

    Delete firebase-app-check.js

--- firebase-app-check.js
+++
@@ -1,8 +0,0 @@
-import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
-
-export function setupAppCheck(firebaseApp) {
-  initializeAppCheck(firebaseApp, {
-    provider: new ReCaptchaV3Provider("6Len7assAAAAABL_triUvCUhGqEn6Nt1DIUeYTNU"),
-    isTokenAutoRefreshEnabled: true
-  });
-}
