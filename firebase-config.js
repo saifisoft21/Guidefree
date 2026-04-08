600b0772f132b0b8204693739c87da6ea5023af8
saifisoft21 <zarvantalks@gmail.com>
2026-04-08 08:48:09 +0100

    Upgrade Firebase SDK to version 10.7.0
    Updated Firebase SDK imports to version 10.7.0.

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyD6ZazsS9ssz_86DFXMwOYA8h_-2ilgxrk",
