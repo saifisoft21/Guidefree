861171733a44911a2f6b5e990c6bbbb26932f316
saifisoft21 <zarvantalks@gmail.com>
2026-04-08 08:49:44 +0100

    Update Firebase SDK version in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
+Import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD6ZazsS9ssz_86DFXMwOYA8h_-2ilgxrk",
